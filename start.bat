26c943574df688c06d86f880d0fdf217f3305319
azgar44 <50982832+azgar44@users.noreply.github.com>
2020-05-09 20:51:04 +0200

    Corrections accents et orthographe du fichier.
    (le "chcp 65001 > nul" permet de mettre les accents)

--- start.bat
+++ start.bat
@@ -1,14 +1,15 @@
 @echo off
+chcp 65001 > nul
 if NOT EXIST node_modules (
-    ECHO Le dossier node_modules n'existe pas dans le repertoire; Installation des modules ...
+    ECHO Le dossier node_modules n'existe pas dans le répertoire; Installation des modules...
     CALL npm install
     if NOT EXIST node_modules (
-        START CMD /C "ECHO Un probleme est survenu lors de l'installation des modules. Assurez-vous que Node (NPM) est installer. && PAUSE"
+        START CMD /C "ECHO Un problème est survenu lors de l'installation des modules. Assurez-vous que Node (NPM) est installé. && PAUSE"
         EXIT
     )
-    ECHO Modules installer.
+    ECHO Modules installés.
 )
 cls
-echo Le bot vas demarrer...
+echo Le bot va démarrer...
 node index.js
 pause
